e0b59ab542dd8d84d7c762e88f3badf8a475c148
Irkan Ahmadov <irkan.ehmedov@gmail.com>
2020-12-28 02:02:58 +0400

    commit

--- ms-zipkin/src/main/java/az/kapitalbank/zipkin/ZipkinApplication.java
+++ ms-zipkin/src/main/java/az/kapitalbank/zipkin/ZipkinApplication.java
@@ -2,7 +2,7 @@ package az.kapitalbank.zipkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import zipkin.server.EnableZipkinServer;
+import zipkin2.server.internal.EnableZipkinServer;
 
 @EnableZipkinServer
 @SpringBootApplication
